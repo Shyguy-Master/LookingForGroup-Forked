d2108aa3012cd9455eb893cbf5e8a5e228e22df6
Judy Derrick <jgd6426@rit.edu>
2024-06-18 17:00:53 -0400

    add more placeholder settings

---
+++ client/src/components/ChangePassword.tsx
@@ -0,0 +1,29 @@
+import { useState, useEffect } from 'react';
+
+const ConfirmDelete = ({ show, onClose, onConfirm }) => {
+    if (!show) {
+        return null;
+    }
+
+    return (
+        <div className="change-pass-modal">
+            <div className="modal">
+                <div className="modal-content">
+                    <h3>Change Password</h3>
+                    <br></br>
+
+                    <input type="password" />
+                    <input type="password" className="password-change" name="oldPassword" placeholder='Old Password' />
+                    <input type="password" className="password-change" name="newPassword" placeholder='New Password' />
+
+                    <div className="modal-buttons">
+                        <button className="btn-cancel" onClick={onClose}>Cancel</button>
+                        <button className="btn-confirm change-pass-btn" onClick={onConfirm}>Change Password</button>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default ConfirmDelete;
--- client/src/components/pages/Settings.tsx
+++ client/src/components/pages/Settings.tsx
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import "./pages.css";
 import ConfirmDelete from '../ConfirmDelete';
+import ChangePassword from '../ChangePassword';
 
 // TO DO:
 // - Add general settings
@@ -14,11 +15,16 @@ import ConfirmDelete from '../ConfirmDelete';
 const Settings = (props) => {
   const [activeTab, setActiveTab] = useState("general");
   const [activeTheme, setActiveTheme] = useState('light'); // State to manage the active theme
+  const [activeColor, setActiveColor] = useState('orange'); // State to manage the active color
   const [showDelete, setShowDelete] = useState(false);
 
   // Function to handle the button clicks and update the h1 text
   const handleThemeChange = (theme) => {
     setActiveTheme(theme);
+  };
+
+  const handleColorChange = (color) => {
+    setActiveColor(color);
   };
 
   const handleDelete = () => {
@@ -56,11 +62,20 @@ const Settings = (props) => {
 
         {/* general settings */}
         <div className={`tab-contents ${activeTab === "general" ? "active-tab" : ""}`} id="general">
-          <div>general settings go here</div>
+          <div>general settings go here,<br></br> these are just some placeholders/examples for now</div>
           <div className="setting">
-            <h5>Theme</h5>
-            <button id="light" className={`theme-btn ${activeTheme === "light" ? "active-theme" : ""}`} onClick={() => handleThemeChange('light')}></button>
-            <button id="dark" className={`theme-btn ${activeTheme === "dark" ? "active-theme" : ""}`} onClick={() => handleThemeChange('dark')}></button>
+            <h5>Main Theme</h5>
+            <button style={{ backgroundColor: 'white', }} className={`theme-btn ${activeTheme === "light" ? "active-theme" : ""}`} onClick={() => handleThemeChange('light')}></button>
+            <button style={{ backgroundColor: 'black', }} className={`theme-btn ${activeTheme === "dark" ? "active-theme" : ""}`} onClick={() => handleThemeChange('dark')}></button>
+          </div>
+          <div className="setting">
+            <h5>Accent Color</h5>
+            <button style={{ backgroundColor: '#FF006D', width: '40px', height: '40px' }} className={`theme-btn ${activeColor === "red" ? "active-theme" : ""}`} onClick={() => handleColorChange('red')}></button>
+            <button style={{ backgroundColor: '#FF7D00', width: '40px', height: '40px' }} className={`theme-btn ${activeColor === "orange" ? "active-theme" : ""}`} onClick={() => handleColorChange('orange')}></button>
+            <button style={{ backgroundColor: '#FFDD00', width: '40px', height: '40px' }} className={`theme-btn ${activeColor === "yellow" ? "active-theme" : ""}`} onClick={() => handleColorChange('yellow')}></button>
+            <button style={{ backgroundColor: '#ADFF02', width: '40px', height: '40px' }} className={`theme-btn ${activeColor === "green" ? "active-theme" : ""}`} onClick={() => handleColorChange('green')}></button>
+            <button style={{ backgroundColor: '#01BEFE', width: '40px', height: '40px' }} className={`theme-btn ${activeColor === "blue" ? "active-theme" : ""}`} onClick={() => handleColorChange('blue')}></button>
+            <button style={{ backgroundColor: '#8F00FF', width: '40px', height: '40px' }} className={`theme-btn ${activeColor === "purple" ? "active-theme" : ""}`} onClick={() => handleColorChange('purple')}></button>
           </div>
           <div className="setting">
             <h5>Language</h5>
@@ -73,13 +88,27 @@ const Settings = (props) => {
           </div>
           <div className="setting">
             <h5>Notifications</h5>
-
+            {/* slider check */}
+            <label className="switch">
+              <input type="checkbox" />
+              <span className="slider round"></span>
+            </label>
           </div>
         </div>
 
         {/* account settings */}
         <div className={`tab-contents ${activeTab === "account" ? "active-tab" : ""}`} id="account">
-          <div>account settings go here</div>
+          <div>account settings go here,<br></br> these are just some placeholders/examples for now</div>
+          <div className="setting">
+            <h5>Password</h5>
+            <input type="password" className="password-change" name="password" value="password" disabled />
+            <button className="change-pass-btn" onClick={handleDelete}> Change Password</button>
+          </div>
+          <ChangePassword
+            show={showDelete}
+            onClose={handleClose}
+            onConfirm={handleConfirmDelete}
+          />
           <div className="setting">
             {/* Delete button: popup asking "are you sure" or require password to confirm */}
             <button className="delete-account-btn" onClick={handleDelete}>Delete Account</button>
