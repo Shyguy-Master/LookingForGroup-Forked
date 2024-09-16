f06e83443c795e3686aaf748761fa8765a0a7415
Shyguy Master <blg0736@gmail.com>
2024-09-16 15:33:47 -0400

    Added page markers
    I added dots below the carousel that makes which item it is currently showing.  You can also click on a dot to scroll to that item.

--- client/src/components/ProjectCarousel.tsx
+++ client/src/components/ProjectCarousel.tsx
@@ -10,46 +10,80 @@ const ProjectCarousel = ({selectedTab, projects, profiles}) => {
     const [currentIndex, setCurrentIndex] = useState(0);
 
     const carouselInfiniteScroll = () => {
-        if (currentIndex === projects.length - 1) {
-            return setCurrentIndex(0);
+        if (selectedTab === 'Projects') {
+            if (currentIndex === projects.length - 1) {
+                return setCurrentIndex(0);
+            }
+            return setCurrentIndex(currentIndex + 1);
         }
-        return setCurrentIndex(currentIndex + 1);
+        else {
+            if (currentIndex === profiles.length - 1) {
+                return setCurrentIndex(0);
+            }
+            return setCurrentIndex(currentIndex + 1);
+        }
     };
 
     useEffect(() => {
         const interval = setInterval(() => {
-            carouselInfiniteScroll()
+            carouselInfiniteScroll();
         }, 5000);
         return () => clearInterval(interval);
     });
 
     if (selectedTab === 'Projects') {
         return (
-            <div className='carousel-container'>
-                {
-                    projects.map((project) => {
-                        return (
-                            <h1 className='carousel-item' style={{transform: `translate(-${currentIndex * 100}%)`}} key={projects.indexOf(project)}>
-                                <ProjectCard project={project}></ProjectCard>
-                            </h1>
-                        );
-                    })
-                }
+            <div className='carousel-section'>
+                <div className='carousel-container'>
+                    {
+                        projects.map((project) => {
+                            return (
+                                <h1 className='carousel-item' style={{transform: `translate(-${currentIndex * 100}%)`}} key={projects.indexOf(project)}>
+                                    <ProjectCard project={project}></ProjectCard>
+                                </h1>
+                            );
+                        })
+                    }
+                </div>
+                <div className='carousel-pages'>
+                    {
+                        projects.map((project, index) => {
+                            return (
+                                <button className={`carousel-dot-${index}`} id={index == currentIndex ? 'current' : ''} key={index} onClick={() => setCurrentIndex(index)}>
+                                    ⬤
+                                </button>
+                            );
+                        })
+                    }
+                </div>
             </div>
         );
     }
     else {
         return (
-            <div className='carousel-container'>
-                {
-                    profiles.map((profile) => {
-                        return (
-                            <h1 className='carousel-item' style={{transform: `translate(-${currentIndex * 100}%)`}} key={profiles.indexOf(profile)}>
-                                <ProfileCard profile={profile}></ProfileCard>
-                            </h1>
-                        );
-                    })
-                }
+            <div className='carousel-section'>
+                <div className='carousel-container'>
+                    {
+                        profiles.map((profile) => {
+                            return (
+                                <h1 className='carousel-item' style={{transform: `translate(-${currentIndex * 100}%)`}} key={profiles.indexOf(profile)}>
+                                    <ProfileCard profile={profile}></ProfileCard>
+                                </h1>
+                            );
+                        })
+                    }
+                </div>
+                <div className='carousel-pages'>
+                    {
+                        profiles.map((profile, index) => {
+                            return (
+                                <button className={`carousel-dot-${index}`} id={index == currentIndex ? 'current' : ''} key={index} onClick={() => setCurrentIndex(index)}>
+                                    ⬤
+                                </button>
+                            );
+                        })
+                    }
+                </div>
             </div>
         );
     }
@@ -58,3 +92,4 @@ const ProjectCarousel = ({selectedTab, projects, profiles}) => {
 export default ProjectCarousel;
 
 // Coding Source: https://blog.bitsrc.io/simple-carousel-in-react-2aac73887243 
+// •●⬤ 
